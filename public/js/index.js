53f5ba7e00a45d49fb9c70b34a2c9cf082c86b2a
joseminaya15 <jose.minayac15@gmail.com>
2018-01-14 09:51:20 -0500

    public

---
+++ public/js/index.js
@@ -0,0 +1,307 @@
+$('#principal .owl-carousel').owlCarousel({
+	lazyLoad : true,
+	responsive : {
+		0 : {
+			items : 1
+		}
+	},
+	navigation : false,
+	nav : false,
+	loop : true,
+	autoplay : true,
+	autoplayTimeout : 3000
+});
+$('#servicios .owl-carousel').owlCarousel({
+	lazyLoad : true,
+	responsive : {
+		0 : {
+			items : 1
+		}
+	},
+	navigation : false,
+	nav : false,
+	loop : true,
+	autoplay : false,
+	autoplayTimeout : 3000
+});
+$('.clientes .owl-carousel').owlCarousel({
+	lazyLoad : true,
+	responsive : {
+		0 : {
+			items : 1
+		},
+		420: {
+			items : 2
+		},
+		768: {
+			items : 3
+		},
+		1200: {
+			items : 4
+		}
+	},
+	navigation : false,
+	nav : false,
+	loop : true,
+	autoplay : false,
+	autoplayTimeout : 3000
+});
+
+$(document).ready(function() {
+	$('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+	});
+	$('.navbar-nav').find('li').click(function(){
+		$('.collapse').toggleClass('in');
+		$('#nav-icon3').toggleClass('open');
+	})
+	resizeContent();
+    $(window).resize(function() {
+        resizeContent();
+    });
+});
+
+var $win = $(window);
+$win.scroll(function() {
+	if ($win.scrollTop() > 55) {
+		$("nav").addClass("navbarcolor");
+		$(".cont-carousel.right").css("top","18%");
+	} else {
+		$("nav").removeClass("navbarcolor");
+		$(".cont-carousel.right").css("top","28%");
+	}
+});
+
+$('a[href^="#"]').click(function(e) {
+ 	var target = $(this).attr('href');
+ 	var strip = target.slice(1);
+ 	var anchor = $("section[id='" + strip + "']");
+ 	e.preventDefault();
+ 	y = (anchor.offset() || {
+ 		"top" : NaN
+ 	}).top;
+ 	$('html, body').animate({
+ 		scrollTop : y
+ 	}, 'slow');
+});
+
+function resizeContent() {
+    var top = $( window ).height();
+	$("body").css('height', top);
+}
+
+// function MenuScrollPintado() {
+// 	if ($("nav").hasClass("pintado")) {
+// 		$("nav").removeClass("pintado");
+// 		if ($win.scrollTop() == 0) {
+// 			$("nav").addClass("navbarcolor");
+// 		}
+// 	} else {
+// 		if ($win.scrollTop() == 0) {
+// 			$("nav").removeClass("navbarcolor");
+// 		}
+// 		$("nav").addClass("pintado");
+// 	}
+// }
+
+function abrirModal(dato) {
+	modal('myModal');
+}
+
+function enviarEmail() {
+	var nombres  = $('#nombre').val();
+	var email    = $('#email').val();
+	var telefono = $('#telefono').val();
+	var mensaje  = $('#mensaje').val();
+
+	if(nombres == '' || nombres == null) {
+		msj('error','Ingrese su nombre completo');
+		return;
+	}
+	if(email == '' || email == null || email == undefined) {
+		msj('error','Ingrese su correo electr&oacute;nico');
+		return;
+	}
+	if (validateEmail(email)) {
+	} else {
+		  msj('error','Este Email: '+email+' no es valido');
+		  return;
+	}
+	if(telefono == '' || telefono == null) {
+		msj('error','Ingrese su teléfono');
+		return;
+	}
+
+	$.ajax({
+		data  : { nombres : nombres,
+				  email   : email,
+				  telefono : telefono,
+				  mensaje : mensaje},
+		url   : 'Inicio/enviarEmail',
+		type  : 'POST',
+		dataType : 'json'
+	}).done(function(data){
+		try{
+			$('#modelo').html('');
+			$('#modelo').append('<option value="">Modelo</option>');
+			$('#modelo').append(data.comboModelo);
+		} catch (err){
+			msj('error',err.message);
+		}
+	});
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function solicitarEstimacion() {
+	var nombre_completo = $('#nombre').val();
+	var celular  		= $('#celular').val();
+	var fecha_llegada	= $('#fecha').val();
+	var hora 	 		= $('#hora').val();
+	var vuelo 	 		= $('#vuelo').val();
+	var servicio 		= $('#servicio').val();
+	var personas		= $('#personas').val();
+	var aerolinea		= $('#aerolinea').val();
+	var correo			= $('#correo').val();
+	var origen			= $('#origen').val();
+	var destino			= $('#destino').val();
+
+	if(nombre_completo == null || nombre_completo == '') {
+		return;
+	}
+	if(celular == null || celular == '') {
+		return;
+	}
+	if(fecha_llegada == null || fecha_llegada == '') {
+		return;
+	}
+	if(hora == null || hora == '') {
+		return;
+	}
+	if(vuelo == null || vuelo == '') {
+		return;
+	}
+	if(servicio == null || servicio == '') {
+		return;
+	}
+	if(personas == null || personas == '') {
+		return;
+	}
+	if(aerolinea == null || aerolinea == '') {
+		return;
+	}
+	if(correo == null || correo == '') {
+		return;
+	}
+	if (!validateEmail(correo)) {
+		return;
+	}
+	if(origen == null || origen == '') {
+		return;
+	}
+	if(destino == null || destino == '') {
+		return;
+	}
+	$.ajax({
+		data  : { nombre_completo : nombre_completo,
+				  celular 	      : celular,
+				  fecha_llegada   : fecha_llegada,
+				  hora 			  : hora,
+				  vuelo 		  : vuelo,
+				  servicio 		  : servicio,
+				  personas 		  : personas,
+				  aerolinea 	  : aerolinea,
+				  correo 	  : correo,
+				  origen 	  : origen,
+				  destino 	  : destino},
+		url   : 'Reserva/solicitarEstimacion',
+		type  : 'POST'
+	}).done(function(data){
+		try{
+        	data = JSON.parse(data);
+        	if(data.error == 0){
+        	}else {
+        		return;
+        	}
+      } catch (err){
+        msj('error',err.message);
+      }
+	});
+}
+
+function soloLetras(e) {
+    key 	   = e.keyCode || e.which;
+    tecla 	   = String.fromCharCode(key).toLowerCase();
+    letras     = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    especiales = "8-37-39-46";
+
+    tecla_especial = false
+    for(var i in especiales){
+         if(key == especiales[i]){
+             tecla_especial = true;
+             break;
+         }
+     }
+     if(letras.indexOf(tecla)==-1 && !tecla_especial){
+         return false;
+     }
+ }
+
+ function valida(e) {
+    tecla = (document.all) ? e.keyCode : e.which;
+    //Tecla de retroceso para borrar, siempre la permite
+    if (tecla==8){
+        return true;
+    }
+    // Patron de entrada, en este caso solo acepta números
+    patron =/[0-9]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function limpiar() {
+
+}
+
+function cambiarNro() {
+	var data = $('#servicio').val();
+	console.log(data);
+	if(data == "Básico") {
+		$('#personas').val("1 - 3");
+		$( "#personas" ).prop( "disabled", true );
+		setTimeout(function(){ 
+			$( "#personas" ).parent().addClass('is-focused');
+		 }, 500);
+	}else if(data == "Élite") {
+		$('#personas').val("4 - 5");
+		$( "#personas" ).prop( "disabled", true );
+		setTimeout(function(){ 
+			$( "#personas" ).parent().addClass('is-focused');
+		 }, 500);
+	}else if(data == "Van") {
+		$('#personas').val("5 - 7");
+		$( "#personas" ).prop( "disabled", true );
+		setTimeout(function(){ 
+			$( "#personas" ).parent().addClass('is-focused');
+		 }, 500);
+	}
+}
+
+/* SCRIPT DE CHAT FACEBOOK */
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id))
+		return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.5";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
---
+++ public/js/jsmenu.js
@@ -0,0 +1,57 @@
+/* MASK DE FECHA */
+function initMaskInputs() {
+	for(var i = 0; i < arguments.length; i++) {
+		var text	= arguments[i];
+		var input	= $('#'+text);
+		input.mask('00/00/0000');
+	}
+}
+function clonarFecha(inputNew,idButton) {
+	$('#'+inputNew.data('time')).val(inputNew.val());
+	$('#'+inputNew.data('time')).focus();
+	$('#'+inputNew.data('time')).blur();
+	if(idButton){
+		$("#"+idButton).trigger("change");
+	}
+}
+$('.mdl-icon .mdl-button').click(function(){
+	$(this).closest('.mdl-input-group').find('.mdl-textfield').addClass('is-dirty');
+});
+//DAYS MAX TODAY
+function initCalendarDaysMaxToday(id, currentDate, fecha){
+	var startDate = new Date();
+	 if (currentDate != undefined) {
+            var startDate = new Date(currentDate);
+        }
+	$("#"+id).bootstrapMaterialDatePicker({ 
+		weekStart : 0, 
+		date	: true, 
+		time	: false, 
+		format 	: 'DD/MM/YYYY',
+		currentDate : startDate,
+		maxDate : (fecha == undefined) ? new Date() : fecha
+	});
+}
+function initButtonCalendarDaysMaxToday(idButton, currentDate, fecha) {
+	var text 		= idButton;
+	var id 			= $("#"+text);
+	var newInput	= null;
+	var iconButton 	= id.closest('.mdl-input').find('.mdl-icon');
+	iconButton.find('.mdl-button').click(function(){
+		newInput = text+'ForCalendar';
+		if ( $('#'+newInput).length < 1 ) {
+			$('<input>').attr({
+			    type		: 'text',
+			    id			: newInput,
+			    name		: newInput,
+			    'data-time'	: text,
+			    onchange 	: 'clonarFecha($(this))',
+			    style		: 'position: absolute; top: 40px; border: transparent; color: transparent; z-index: -4'
+			}).appendTo(iconButton);
+			initCalendarDaysMaxToday(newInput, currentDate, fecha);
+		}
+		$("#"+newInput).focus();			
+	});		
+	var valueNewInput = $("#"+newInput).val();   
+	id.text(valueNewInput);
+}
