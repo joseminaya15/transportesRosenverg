a749f17ddddef9413fdcab7d975e347d9028c8bd
joseminaya15 <jose.minayac15@gmail.com>
2018-01-20 15:39:31 -0500

    utils

--- public/js/Utils.js
+++ public/js/Utils.js
@@ -536,62 +536,6 @@ function isDate(txtDate) {
 	};
 }(jQuery));
 
-/*
- * function marcarNodo(nodo){ $.ajax({ url: "setNodoSession", data: { nodo :
- * nodo}, async : false, type: 'POST' }) .done(function(data){ }); }
- */
-
-function toggleFullScreen() {
-	if ((document.fullScreenElement && document.fullScreenElement !== null)
-			|| (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-		if (document.documentElement.requestFullScreen) {
-			document.documentElement.requestFullScreen();
-		} else if (document.documentElement.mozRequestFullScreen) {
-			document.documentElement.mozRequestFullScreen();
-		} else if (document.documentElement.webkitRequestFullScreen) {
-			document.documentElement
-					.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-		}
-		$('#icon_fullScreen').removeClass('md-fullscreen');
-		$('#icon_fullScreen').addClass('md-fullscreen-exit');
-	} else {
-		if (document.cancelFullScreen) {
-			document.cancelFullScreen();
-		} else if (document.mozCancelFullScreen) {
-			document.mozCancelFullScreen();
-		} else if (document.webkitCancelFullScreen) {
-			document.webkitCancelFullScreen();
-		}
-		$('#icon_fullScreen').removeClass('md-fullscreen-exit');
-		$('#icon_fullScreen').addClass('md-fullscreen');
-	}
-}
-
-var menuC = 0;
-function changeIconMenu() {
-	if (menuC == 0) {
-		$('#iconMenu').removeClass('giro1');
-		$('#iconMenu').removeClass('md md-menu');
-		$('#iconMenu').addClass('md md-clear');
-		$('#iconMenu').addClass('giro');
-
-		menuC = 1;
-	} else {
-		$('#iconMenu').removeClass('giro');
-		$('#iconMenu').removeClass('md md-clear');
-		$('#iconMenu').addClass('md md-menu');
-		$('#iconMenu').addClass('giro1');
-		menuC = 0;
-	}
-}
-
-function cerrarMenu() {
-	$('body').removeClass('menubar-visible');
-}
-
-function abrirMenu() {
-	$('body').addClass('menubar-visible');
-}
 
 function successValidConfig(idTabla, indexRow, indexCampo, pk, nuevoValor, msj, clase, idGrupo, idNota) {
 	$('#' + idTabla).bootstrapTable(
@@ -620,60 +564,6 @@ function successValid(idTabla, indexRow, indexCampo, pk, nuevoValor, msj, clase)
 function marcarNodo(id) {
 	$("#" + id).addClass("active");
 	$("#" + id).find("a").addClass("active");
-}
-
-function openModalFeedBack(){
-	$('#navBar').removeClass('is-visible');
-	$('.mdl-layout__obfuscator').removeClass('is-visible');
-	modal('modalFeedBack');
-}
-
-
-function enviarFeedback(ruta, tipo, rutaOrigen) {
-	var txtArea = (tipo == 'COL' ? 'feedbackMsjColegio' : 'feedbackMsjSmiledu');
-	if(tipo == 'COL') {
-		addLoadingButton('btnColegioFeedback');	
-	} else if(tipo == 'SMILEDU') {
-		addLoadingButton('btnSmileduFeedback');
-	}
-	var msj = $("#"+txtArea).val();
-	if (msj.trim() != "") {
-		modal('modalFeedBackTY');
-		var valorSede = $("#sedeFeed").val();
-		var hijos = ''; //$("#sedeFeed").text();//console.log(hijos);
-		$("#sedeFeed option:selected").each(function () {
-			var $this = $(this);
-		    if ($this.length) {
-			    hijos += $this.text()+'|';
-		    }
-		});
-		$.ajax({
-			data : { feedbackMsj : msj,
-					 url         : rutaOrigen,
-					 valorSede   : valorSede,
-					 hijos       : hijos,
-					 tipo        : tipo,
-					 client_info : datosClient },
-			url : ruta,
-			type : 'POST'
-		}).done(function(data) {
-			data = JSON.parse(data);
-			$("#feedbackMsjSmiledu,#feedbackMsjColegio").val("");
-			mostrarNotificacion('success',data.msj);
-			//abrirCerrarModal('modalFeedBack');
-			stopLoadingButton('btnSmileduFeedback');
-			stopLoadingButton('btnColegioFeedback');
-		});
-	}
-}
-
-function openModalMisionVision() {
-	abrirCerrarModal("modalMisionVision");
-}
-
-function goToPerfilUsuario(data) {
-	window.location.href = window.location.origin
-			+ '/smiledu/c_perfil?usuario=' + data;
 }
 
 function setearInput(idInput, val, previo, disabled, clase) {
@@ -779,95 +669,6 @@ function isChecked(element){
 	return tof;
 }
 
-function disableEnableInput(idInput, tof){
-	$('#'+idInput).attr("disabled", tof);
-	if(tof == false){
-		$('.divInput').removeClass('is-disabled');
-		$("#"+idInput).css('cursor', '');
-	}else{
-		$("#"+idInput).css('cursor', 'not-allowed');
-	}
-}
-
-function reintentarBusqueda(){
-	$("#searchMagic").focus();
-	$("#searchMagic").select();
-}
-
-function tableEventsUpgradeMdlComponentsMDL(idTable){
-	$(function () {
-	    $('#'+idTable).on('all.bs.table', function (e, name, args) {
-			$(document).ready(function(){
-			    $('[data-toggle="tooltip"]').tooltip(); 
-		    });
-			componentHandler.upgradeAllRegistered();
-	    })
-	    .on('click-row.bs.table', function (e, row, $element) {
-	    })
-	    .on('dbl-click-row.bs.table', function (e, row, $element) {
-			$(document).ready(function(){
-			    $('[data-toggle="tooltip"]').tooltip(); 
-		    });
-			componentHandler.upgradeAllRegistered();
-	    })
-	    .on('sort.bs.table', function (e, name, order) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-	    })
-	    .on('check.bs.table', function (e, row) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-
-	    })
-	    .on('uncheck.bs.table', function (e, row) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-	    })
-	    .on('check-all.bs.table', function (e) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-	    })
-	    .on('uncheck-all.bs.table', function (e) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-
-	    })
-	    .on('load-success.bs.table', function (e, data) {
-
-	    })
-	    .on('load-error.bs.table', function (e, status) {
-
-	    })
-	    .on('column-switch.bs.table', function (e, field, checked) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-	    })
-	    .on('page-change.bs.table', function (e, size, number) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-	    })
-	    .on('search.bs.table', function (e, text) {
-	    	$(document).ready(function(){
-	    	    $('[data-toggle="tooltip"]').tooltip(); 
-	        });
-	    	componentHandler.upgradeAllRegistered();
-	    });
-	});
-}
 
 String.prototype.initCap = function () {
     return this.toLowerCase().replace(/(?:^|\s)[a-z]/g, function (m) {
@@ -879,21 +680,6 @@ Number.prototype.round = function(places) {
 	return +(Math.round(this + "e+" + places)  + "e-" + places);
 }
 
-function getFechaHoy_dd_mm_yyyy() {
-	var d = new Date();
-	var mes = (d.getMonth()+1+'').length === 1 ? '0'+(d.getMonth()+1) : (d.getMonth()+1);
-	var dia = (d.getDate()+'').length === 1 ? '0'+d.getDate() : (d.getDate());
-	var hoyDia = dia+'/'+mes+'/'+d.getFullYear();
-	return hoyDia;
-}
-
-function setMultiCombo(idNameCombo, valores,selected) {
-	$('#' + idNameCombo).find('option').remove().end().append(valores);
-	if(selected == undefined){
-		$('select[name=' + idNameCombo + ']').val("");
-	}
-	$('#' + idNameCombo).selectpicker('refresh');
-}
 
 function readable(bytes, precision) {
 	var kilobyte = 1024, megabyte = kilobyte * 1024, gigabyte = megabyte * 1024, terabyte = gigabyte * 1024;
@@ -911,26 +697,6 @@ function readable(bytes, precision) {
 	} else {
 		return bytes + ' B';
 	}
-}
-
-function getBase64Image(img) {
-	var canvas = document.createElement("canvas");
-	canvas.width  = img.width;
-	canvas.height = img.height;
-	var ctx = canvas.getContext("2d");
-	ctx.drawImage(img, 0, 0);
-	var dataURL = canvas.toDataURL("image/jpeg");
-	return dataURL;
-} 
-
-function getBase64ImagePNG(img) {
-	var canvas = document.createElement("canvas");
-	canvas.width  = img.width;
-	canvas.height = img.height;
-	var ctx = canvas.getContext("2d");
-	ctx.drawImage(img, 0, 0);
-	var dataURL = canvas.toDataURL("image/png");
-	return dataURL;
 }
 
 var addEvent = (function () {
@@ -1050,134 +816,6 @@ function setearComboAdm(idCombo, val){
 	}
 }
 
-function cambioCampoAdm(element){
-	val = $(element).val();
-	if(val != null && val.length != 0 && val != 0){
-		$(element).closest(".mdl-input-group").removeClass("is-invalid");
-	} else {
-		$(element).closest(".mdl-input-group").addClass("is-invalid");
-	}
-}
-
-function getFechaTexto(fecha) {
-	var texto = null;
-	try {
-		var monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-		                  "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"];
-		if(fecha == null) {
-			fecha = new Date();
-		} else {
-			fecha = new Date(fecha);
-		}
-		texto = fecha.getDate()+' de '+monthNames[fecha.getMonth()]+' del '+fecha.getFullYear();
-	} catch(err) {
-		msj('error', err.message);
-		texto = null;
-	}
-	return texto;
-}
-
-function cambiarRol(aElement) {
-	var urlSmiledu  = window.location.protocol+'//'+window.location.hostname+'/';
-	var arrayUrl    = (window.location.pathname).split('/');
-	$('#idRolChange').val(aElement.attr('attr-rol'));
-	$('#urlRedirect').val(urlSmiledu+arrayUrl[1]+'/'+arrayUrl[2]+'/c_main');
-	$('#sistemaActi').val(arrayUrl[2]);
-	$('#formCambioRol').submit();
-}
-
-function getComboYearActual(id) {
-	var fecha = new Date();
-	var year = fecha.getFullYear();
-
-	$("#"+id+" option").filter(function() {
-	    return this.text == year; 
-	}).attr('selected', true);
-	$("#"+id).selectpicker('render');
-}
-
-/*Realiza una resta y devuelve la rpta en minutos 
- *@Jhonatan Meza
- */
-function restarHoras(horaInicio,horaFin){
-	var inicio = convertirFormatoHora(horaInicio);
-	var fin    = convertirFormatoHora(horaFin);
-    inicio = inicio.split(':');
-    fin    = fin.split(':');
-	var nInicio = parseInt(inicio[0])*60;
-	var nFin    = parseInt(fin[0])*60;
-	nInicio = nInicio+parseInt(inicio[1]);
-	nFin    = nFin+parseInt(fin[1]);
-	return nFin-nInicio;
-}
-
-/*valida con un boolean si la fecha es menor = 1 sino = 0*/
-/*@Jhonatan Meza*/
-function validarFechaMenorActual(date){
-    var x=new Date();
-    var fecha = date.split("/");
-    x.setFullYear(fecha[2],fecha[1]-1,fecha[0]);
-    var today = new Date();
-
-    if (x > today)
-      return false;
-    else
-      return true;
-}
-
-/*d  = dias a sumar, fecha = fecha que desea sumarle los dias*/
-/*@Jhonatan Meza*/
-function sumaFecha(d, fecha)
-{
-	var Fecha = new Date();
-	var sFecha = fecha || (Fecha.getDate() + "/" + (Fecha.getMonth() +1) + "/" + Fecha.getFullYear());
-	var sep = sFecha.indexOf('/') != -1 ? '/' : '-'; 
-	var aFecha = sFecha.split(sep);
-	var fecha = aFecha[2]+'/'+aFecha[1]+'/'+aFecha[0];
-	fecha= new Date(fecha);
-	fecha.setDate(fecha.getDate()+parseInt(d));
-	var anno=fecha.getFullYear();
-	var mes= fecha.getMonth()+1;
-	var dia= fecha.getDate();
-	mes = (mes < 10) ? ("0" + mes) : mes;
-	dia = (dia < 10) ? ("0" + dia) : dia;
-  	var fechaFinal = dia+sep+mes+sep+anno;
-  	return (fechaFinal);
- }
-
-//Funcion que devuelve un true si la fecha a validar esta en rango de 2 fechas, sino false
-/*@Jhonatan Meza*/
-function valueFechaRangoFechas(valuefecha,fecha1,fecha2){
-	 var sep = valuefecha.indexOf('/') != -1 ? '/' : '-'; 
-	 var aFecha = valuefecha.split(sep);
-	 var fecha = aFecha[2]+'/'+aFecha[1]+'/'+aFecha[0];
-	 valueFecha= new Date(fecha); 
-	 var sep1 = fecha1.indexOf('/') != -1 ? '/' : '-'; 
-	 var aFecha1 = fecha1.split(sep1);
-	 var fecha1 = aFecha1[2]+'/'+aFecha1[1]+'/'+aFecha1[0];
-	 Fecha1= new Date(fecha1);
-	 var sep2 = fecha2.indexOf('/') != -1 ? '/' : '-'; 
-	 var aFecha2 = fecha2.split(sep2);
-	 var fecha2 = aFecha2[2]+'/'+aFecha2[1]+'/'+aFecha2[0];
-	 Fecha2= new Date(fecha2);	 
-	 if(valueFecha>=Fecha1 && valueFecha<=Fecha2){
-		 return true;		 
-	 }else{
-		 return false;
-	 }
-}
-
-function isHour(valueHour){
-	var hour   = parseInt(valueHour.substr(0,3));
-	var minute = parseInt(valueHour.substr(3,4));
-	var valor = null;
-	if(hour<24 && minute<60){
-		return true;		
-	}else{
-		return false;
-	}
-}
-
 var date_regex = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
 
 function validate_fechaMayorQue(fechaInicial,fechaFinal){
@@ -1191,13 +829,6 @@ function validate_fechaMayorQue(fechaInicial,fechaFinal){
         return 0;
     }
     return 1;
-}
-
-function justNumbers(e) {
-   var keynum = window.event ? window.event.keyCode : e.which;
-   if ((keynum == 8) || (keynum == 46))
-        return true;
-    return /\d/.test(String.fromCharCode(keynum));
 }
 
 function get_elapsed_time_string(total_seconds) {
@@ -1413,30 +1044,4 @@ function array_column(arry, column, indice) {
         }
     }
     return result;
-}
-
-/**
- * Refresca los tabs
- * @author : jmeza
- * @param  : contTabBar   Id del contenedor de los tabs bar con   "#" 
- * @param  : contTabPanel Id del contenedor de los tabs panel con "#"
- * @param  : seleted      Href del tab a seleccionar sin el       "#"
- */
-function refreshTabActive(contTabBar, contTabPanel, selected){
-	var bars   = $(contTabBar).find('a');
-	var panels = $(contTabPanel).find('.mdl-tabs__panel');
-	$.each(bars, function(index,val){
-		if($(val).attr('href') == ('#'+selected)){
-			$(val).addClass('is-active');
-		} else{
-			$(val).removeClass('is-active');
-		}
-	});
-	$.each(panels, function(index,val){
-		if($(val).attr('id') == selected){
-			$(val).addClass('is-active');
-		} else{
-			$(val).removeClass('is-active');
-		}
-	});
 }
